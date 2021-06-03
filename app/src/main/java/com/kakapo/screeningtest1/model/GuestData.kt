368e4c40fb5e78507a9e98bc6f2fc8d8de821dda
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-06-03 13:30:09 +0700

    Initial commit

---
+++ app/src/main/java/com/kakapo/screeningtest1/model/GuestData.kt
@@ -0,0 +1,7 @@
+package com.kakapo.screeningtest1.model
+
+data class GuestData(
+    val id: Int,
+    val name: String,
+    val birthDate: String
+)
---
+++ app/src/main/java/com/kakapo/screeningtest1/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kakapo.screeningtest1.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kakapo.screeningtest1.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
